9e7243c7b67b26b84eb6d1d11c7b420030091a4e
nelsonlopez500 <nelson.lopez@catolica.edu.sv>
2025-03-24 20:15:54 -0600

    Agregar archivos de proyecto.

---
+++ VistasyVariablesdeSesion/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VistasyVariablesdeSesion.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
