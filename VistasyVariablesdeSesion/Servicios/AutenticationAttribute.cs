5311142f0846cfaaf0296696e562c16e1af19657
nelsonlopez500 <nelson.lopez@catolica.edu.sv>
2025-03-24 21:27:08 -0600

    ActionFilterAttribute

---
+++ VistasyVariablesdeSesion/Servicios/AutenticationAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace VistasyVariablesdeSesion.Servicios
+{
+    public class AutenticacionAttribute : ActionFilterAttribute
+    {
+        // Este método se ejecuta antes de que se ejecute la acción del controlador
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            // Verificar si la variable de sesión 'UsuarioId' existe
+            var usuarioId = context.HttpContext.Session.GetInt32("UsuarioId");
+
+            if (usuarioId == null)
+            {
+                // Si no está autenticado, redirigir a la página de login
+                context.Result = new RedirectToActionResult("Autenticar", "Home", null);
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
